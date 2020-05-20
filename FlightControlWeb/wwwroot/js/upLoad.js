f2e013452fc04403168fdbd41dd92f48cb89c03c
Roei Ben zeev <roeibenzeev@gmail.com>
2020-05-21 01:17:35 +0300

    for check

--- FlightControlWeb/wwwroot/js/upLoad.js
+++ FlightControlWeb/wwwroot/js/upLoad.js
@@ -1,78 +1,37 @@
 
+var uploadButton = document.getElementById('inputFiles');
 
-(function () {
-    "use strict";
-    var dropZone = document.getElementById('dropzone');
 
-    //drag over drop zone handler
-    dropZone.ondragover = function() {
-        this.className = 'dropzone-dragover';
-        return false; 
-    }
-    
-    //drag leave case handler
-    dropZone.ondragleave = function() {
-        this.className = 'dropzone';
-        return false; 
-    }
 
-    function handleFiles(files) {
-        ([...files]).forEach(upload)
-    }
+function readFile(file) {
+    //we define a reader to read input file
+    let reader = new FileReader();
+    //read file as text
+    reader.readAsText(file);
+    //initial variable to hold out data from reader
+    let data;
 
-    const toSend = {
-        "passenger": 150,
-        "company_name": "SwissAir",
-        "initial_location": {
-            "longitude": 20.0,
-            "latitude": 30.2,
-            "date_time": "2020-12-27T01:56:21Z"
-        },
-        "segments": [
-            {
-                "longitude": 33.23,
-                "latitde": 31.56,
-                "timespan_seconds": 850.0
-            }
-        ]
+    reader.onload = function () {
+        data = reader.result.replace('/r', '');
+        console.log(data);
     };
 
-    //upload to server function
-    var upload = function (files) {
-        var formData = new FormData(),
-            xhr = new XMLHttpRequest(),
-            x;
-        for(x=0 ; x < files.length ; x= x+1) {
-            formData.append('file[]', files[x]);
-        }
-       
-        ////event handler to check if trasfer succeed
-        xhr.onload = function() {
-            var data = this.responseText;
-            console.log(data);
-        }
-       
-        //open our connection
-        xhr.open('post', 'api/FlighPlan');
-        //xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(formData);
+}
+
+
+
+//this function first iterate our files and send them to read file function
+uploadButton.onchange =  function() {
+
+    let files = uploadButton.files;
+    let file;
+
+    var x;
+
+    for (x = 0; x < files.length; x = x + 1) {
+        file = files[x];
+        readFile(file);
     }
-    let test = function (files) {
-        let file = files[0];
 
-        let reader = new FileReader();
+}
 
-        reader.readAsText(file);
-
-        console.log(file);
-    };
-
-    //on file drop case handler
-    dropZone.ondrop = function(e) {
-        e.preventDefault();
-        this.className = 'dropzone';
-        var data = e.dataTransfer.getData("Text");
-        //console.log(e.dataTransfer.files);
-        test(e.dataTransfer.files);
-    }
-}());
